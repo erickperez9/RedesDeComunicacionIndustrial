5595b8338e2ebd2e346ce5e86a02578c279e4c6d
erickperez9 <116017901+erickperez9@users.noreply.github.com>
2023-11-27 15:40:41 -0500

    Add files via upload

---
+++ Códigos Prácticas/Práctica 1.19/modbus rtu mduino/rs232_master_coils/rs232_master_coils.ino
@@ -0,0 +1,63 @@
+#include <ModbusRTUMaster.h>
+#include <RS232.h>
+
+ModbusRTUMaster master(RS232);
+
+#define COILS_TO_WRITE 5
+uint32_t lastSentTime = 0UL;
+const uint32_t baudrate = 38400UL;
+
+void setup() {
+  Serial.begin(9600UL);
+  RS232.begin(baudrate, SERIAL_8E1);
+  // Start the modbus master object.
+  // It is possible to define the port rate (default: 19200)
+  master.begin(baudrate);
+}
+
+void loop() {
+  // Send a request every 1000ms
+  if (millis() - lastSentTime > 1000) {
+    
+    // Set random values
+    bool values[5];
+    for (int i = 0; i < 5; ++i) {
+      values[i] = random() & 0x01;
+    }
+
+    // Send a Write Multiple Coils request to the slave with address 31
+    // It requests for setting COILS_TO_WRITE coils starting in address 0
+    // IMPORTANT: all read and write functions start a Modbus transmission, but they are not
+    // blocking, so you can continue the program while the Modbus functions work. To check for
+    // available responses, call modbus.available() function often.
+    if (!master.writeMultipleCoils(31, 0, values, COILS_TO_WRITE)) {
+      // Failure treatment
+      Serial.println("Request fail");
+    }
+
+    lastSentTime = millis();
+  }
+
+  // Check available responses often
+  if (master.isWaitingResponse()) {
+    ModbusResponse response = master.available();
+    if (response) {
+      if (response.hasError()) {
+        // Response failure treatment. You can use response.getErrorCode()
+        // to get the error code.
+        Serial.print("Error ");
+        Serial.println(response.getErrorCode());
+      } else {
+        // Get the discrete inputs values from the response
+        if (response.hasError()) {
+          // Response failure treatment. You can use response.getErrorCode()
+          // to get the error code.
+          Serial.print("Error ");
+          Serial.println(response.getErrorCode());
+        } else {
+          Serial.println("Done");
+        }
+      }
+    }
+  }
+}
