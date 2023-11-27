1b09906295db7ec705d3aa69ef65d497359f6182
erickperez9 <116017901+erickperez9@users.noreply.github.com>
2023-11-27 15:40:16 -0500

    Add files via upload

---
+++ Códigos Prácticas/Práctica 1.17/intro a mduino 21/blink/blink.ino
@@ -0,0 +1,24 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(Q0_0, HIGH);//Writing a HIGH on a Digital Output
+  delay(500);
+  digitalWrite(Q0_0, LOW);//Writing a HIGH on a Digital Output
+  delay(500);
+  digitalWrite(Q0_2, HIGH);//Writing a HIGH on a Digital Output
+  delay(500);
+  digitalWrite(Q0_2, LOW);//Writing a HIGH on a Digital Output
+  delay(500);
+  digitalWrite(Q0_4, HIGH);//Writing a HIGH on a Digital Output
+  delay(500);
+  digitalWrite(Q0_4, LOW);//Writing a HIGH on a Digital Output
+  delay(500);
+  digitalWrite(Q0_6, HIGH);//Writing a HIGH on a Digital Output
+  delay(500);
+  digitalWrite(Q0_6, LOW);//Writing a HIGH on a Digital Output
+  delay(500);
+}
